639415bc77a9b370f3405313ae8eec03300e94b8
Oleksandr Popov <31711254+Popooov@users.noreply.github.com>
2022-02-20 11:08:22 +0000

    Fix some minor errors

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css';
-import * as serviceWorker from './serviceWorker'
+import './index.css'
 import Nav from './components/Nav'
 import Loading from './components/Loading'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
@@ -40,9 +39,4 @@ function App() {
 }
   
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<App />, document.getElementById('root'))
